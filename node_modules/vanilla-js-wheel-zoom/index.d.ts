1660e8355cbaf1a6c55b0a03e4546260bf0910b8
Logan-Ward <ward_logan@outlook.com>
2023-04-23 19:05:38 -0500

    dev: axios update

--- node_modules/vanilla-js-wheel-zoom/index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'vanilla-js-wheel-zoom';
--- node_modules/vanilla-js-wheel-zoom/prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    bracketSpacing: true,
-    trailingComma: 'es5',
-    bracketSameLine: false,
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-};
